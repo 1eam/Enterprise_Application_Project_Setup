02ae87a901d310362f8125259f4fb0473aa7c855
Esther <esther191299@gmail.com>
2020-12-14 03:52:36 +0100

    created first test-endpoint in Userservice api and Used .yml file to set Eurekas defaultZone instead of .properties file

--- Users Service API/src/main/java/com/enterpriseapp/users_service_api/UsersServiceApiApplication.java
+++ Users Service API/src/main/java/com/enterpriseapp/users_service_api/UsersServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.enterpriseapp.users_service_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UsersServiceApiApplication {
 
     public static void main(String[] args) {
