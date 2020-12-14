f51fba6a878f1679cc0dd52609e80fe2f8234b97
Esther <esther191299@gmail.com>
2020-12-14 01:40:17 +0100

    Initialized User Service API

---
+++ Users Service API/src/test/java/com/enterpriseapp/users_service_api/UsersServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterpriseapp.users_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
