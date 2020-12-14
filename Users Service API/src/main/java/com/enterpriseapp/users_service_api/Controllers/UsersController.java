d54e050cfdb3de97d33f8ddb6740f6752c978153
Esther <esther191299@gmail.com>
2020-12-14 23:46:15 +0100

    Written testcode in Users-Service to return instance's portnumber to test loadbalancer to be working

--- Users Service API/src/main/java/com/enterpriseapp/users_service_api/Controllers/UsersController.java
+++ Users Service API/src/main/java/com/enterpriseapp/users_service_api/Controllers/UsersController.java
@@ -1,14 +1,18 @@
 package com.enterpriseapp.users_service_api.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status")
     public String checkStatus(){
-        return "User Controller Working...";
+        return "User Controller Working on port " + environment.getProperty("local.server.port");
     }
 }
