fa8b4fd2c9287d4b3b9ba628fd90ec45c8306798
Esther <esther191299@gmail.com>
2020-12-16 05:18:43 +0100

    created postmapping stump in UsersController

--- Users Service API/src/main/java/com/enterpriseapp/users_service_api/Controllers/UsersController.java
+++ Users Service API/src/main/java/com/enterpriseapp/users_service_api/Controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.enterpriseapp.users_service_api.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class UsersController {
     @GetMapping("/status")
     public String checkStatus(){
         return "User Controller Working on port " + environment.getProperty("local.server.port");
+    }
+
+    @PostMapping("/users")
+    public String createUser(){
+        return "Create user method was called";
     }
 }
