c94d3ebcdb7e79d5ec8e45eeebbdd9f5e88657b3
Esther <esther191299@gmail.com>
2020-12-20 00:01:11 +0100

    Added set Userid using randomUUID

--- Users Service API/src/main/java/com/enterpriseapp/users_service_api/service/UserServiceImpl.java
+++ Users Service API/src/main/java/com/enterpriseapp/users_service_api/service/UserServiceImpl.java
@@ -1,4 +1,11 @@
 package com.enterpriseapp.users_service_api.service;
 
-public class UserServiceImpl {
+import java.util.UUID;
+
+public class UserServiceImpl implements IUserService {
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+        return null;
+    }
 }
