838d02beefdbd30b10c5fca273cdf42916584dfa
Esther <esther191299@gmail.com>
2020-12-19 05:08:36 +0100

    Added validation-constraints on user-registration model

--- Users Service API/src/main/java/com/enterpriseapp/users_service_api/controllers/UsersController.java
+++ Users Service API/src/main/java/com/enterpriseapp/users_service_api/controllers/UsersController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class UsersController {
 
@@ -20,7 +22,7 @@ public class UsersController {
     }
 
     @PostMapping("/users")
-    public String createUser(@RequestBody UserRegistrationModel userInput){
-        return "Create user method was called";
+    public String createUser(@Valid @RequestBody UserRegistrationModel userInput){
+        return userInput.getEmail();
     }
 }
--- Users Service API/src/main/java/com/enterpriseapp/users_service_api/model/UserRegistrationModel.java
+++ Users Service API/src/main/java/com/enterpriseapp/users_service_api/model/UserRegistrationModel.java
@@ -1,9 +1,25 @@
 package com.enterpriseapp.users_service_api.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserRegistrationModel {
+    @NotNull(message = "First name field is Empty \n Please enter your first name")
+    @Size(min = 2, max = 32, message = "Last name field-size should at least contain 2 and a maximum of 32 characters")
     private String firstName;
+
+    @NotNull(message = "Last name field is Empty \n Please enter your last name")
+    @Size(min = 2, max = 100, message = "Last name field-size should at least contain 2 and a maximum of 100 characters")
     private String lastName;
+
+    @NotNull(message = "Email field is Empty \n Please enter your email name")
+    @Email(message = "invalid email input")
+    @Size(min = 2, max = 100, message = "email size exceeds on an input size over 100")
     private String email;
+
+    @NotNull(message = "Password field is Empty \n Please enter a password name")
+    @Size(min = 8, max = 32, message = "Password-size should be between 8 - 32")
     private String password;
 
     public String getFirstName() {
