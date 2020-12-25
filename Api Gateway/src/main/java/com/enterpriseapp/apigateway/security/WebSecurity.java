9bc5de00a53cb687c73eda1ff0a7c8962c72431d
Esther <esther191299@gmail.com>
2020-12-25 06:00:15 +0100

    Added Spring security and JWT dependencies + WebSecurity class stub

---
+++ Api Gateway/src/main/java/com/enterpriseapp/apigateway/security/WebSecurity.java
@@ -0,0 +1,10 @@
+package com.enterpriseapp.apigateway.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+}
