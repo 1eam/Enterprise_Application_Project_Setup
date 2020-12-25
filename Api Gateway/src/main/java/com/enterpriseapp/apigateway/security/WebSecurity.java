753c9a46e91b131925467ae6222da67f1fb420cd
Esther <esther191299@gmail.com>
2020-12-25 07:10:00 +0100

    Configured permits and Authorizations on Zuul API Gateway for Users-microservice + changed user registration post method on /users to /registration

--- Api Gateway/src/main/java/com/enterpriseapp/apigateway/security/WebSecurity.java
+++ Api Gateway/src/main/java/com/enterpriseapp/apigateway/security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.enterpriseapp.apigateway.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,6 +23,14 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         //disables csrf and allows for h2 db to be seen
         http.csrf().disable();
         http.headers().frameOptions().disable();
+
+        //allows http requests without authorization to the following paths: (values of env.getProperty method)
+        http.authorizeRequests()
+                .antMatchers(environment.getProperty("api.h2Console.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
+                //finally any other request should be authenticated
+                .anyRequest().authenticated();
 
         //prohibits microservice (Spring) from creating http session, makes this API stateless to avoid cashing
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
