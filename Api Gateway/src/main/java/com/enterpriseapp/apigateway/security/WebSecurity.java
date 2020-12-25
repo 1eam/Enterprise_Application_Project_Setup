b0d7897e952eb28c2320a207b31bec9f13a76cd6
Esther <esther191299@gmail.com>
2020-12-25 06:08:16 +0100

    Added Environment dependency (injection)

--- Api Gateway/src/main/java/com/enterpriseapp/apigateway/security/WebSecurity.java
+++ Api Gateway/src/main/java/com/enterpriseapp/apigateway/security/WebSecurity.java
@@ -1,10 +1,23 @@
 package com.enterpriseapp.apigateway.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final Environment environment;
+
+    public WebSecurity(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
 }
