d34eb96fb0c89d797bd27d2d1e8c6b2554e612e5
Esther <esther191299@gmail.com>
2021-01-09 04:08:09 +0100

    removed Cucumber stepdefs because they were not working anymore

--- Email Verification Service/src/test/java/com/enterpriseapp/email_verification_service/controllers/EmailVerificationControllerTest.java
+++ Email Verification Service/src/test/java/com/enterpriseapp/email_verification_service/controllers/EmailVerificationControllerTest.java
@@ -6,7 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EmailVerificationControllerTest {
 
+    EmailVerificationController emailVerificationController = new EmailVerificationController();
+
     @Test
-    void checkStatus() {
+    void test_checkStatusMethod() {
+
+        String expected = "Email Verification Controller working...";
+        String actual = emailVerificationController.checkStatus();
+        assertEquals(expected, actual);
     }
 }
