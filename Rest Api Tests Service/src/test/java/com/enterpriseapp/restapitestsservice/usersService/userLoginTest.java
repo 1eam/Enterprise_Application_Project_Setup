00c7a1ef5ca408e266a0be7f521eb69059bbc4de
Esther <esther191299@gmail.com>
2021-01-12 07:57:29 +0100

    completed login results tests: checking if userId and token are present + statuscode 200 response

--- Rest Api Tests Service/src/test/java/com/enterpriseapp/restapitestsservice/usersService/userLoginTest.java
+++ Rest Api Tests Service/src/test/java/com/enterpriseapp/restapitestsservice/usersService/userLoginTest.java
@@ -10,50 +10,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DisplayName("Should test Users-Service - regarding login")
 public class userLoginTest {
 
-//    private final String context_path = "users-service-api";
-//    private String authorizationHeader = "Authorization";
-//    private String token = "Bearer " + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1N2M2NTViNS02ZjA2LTQxNmUtYjhiYi02OTkzYTJkNDMzOGQiLCJleHAiOjE2MTEwMTc1NDF9.1bDiHHKTR1t7LRiOeuzcQfzrF1o6iZ6QPnh7jIOT2KWY4B4Qoq_MI75BuIqgOpxfo4otaLEgtOYPwLqhFxO7rA";
-//
-//    @BeforeEach
-//    void setup() throws Exception{
-//        RestAssured.baseURI = "http://192.168.192.47";
-//        RestAssured.port = 8011;
-//    }
-//
-//    @Test //POST
-//    @DisplayName("Should test, [to /register] - expected registration-response + respond with 201")
-//    final void testRegistrationResponse(){
-//
-//        //What we want to send in the body
-//        Map<String, Object> registrationCredentials = new HashMap<>();
-//        registrationCredentials.put("firstName", "Esther");
-//        registrationCredentials.put("lastName", "Tester");
-//        registrationCredentials.put("email", "esther@gmail.com");
-//        registrationCredentials.put("password", "12345678");
-//
-//
-//        Response response =
-//                given()
-//                        .contentType("application/json")
-//                        .accept("application/json")
-//                        .body(registrationCredentials)
-//                        .when()
-//                        .post(context_path + "/register")
-//                        .then()
-//                        .statusCode(201)
-//                        .contentType("application/json")
-//                        .extract()
-//                        .response();
-//
-//        //Also test response object is not empty, and has id size equal to 36
-//        String userId = response.jsonPath().getString("userId");
-//        assertNotNull(userId);
-//        assertEquals(userId.length(), 36);
-//    }
+    private final String context_path = "users-service-api";
+
+    @BeforeEach
+    void setup() throws Exception{
+        RestAssured.baseURI = "http://192.168.192.47";
+        RestAssured.port = 8011;
+    }
+
+    @Test //POST
+    @DisplayName("Should test, [to /login], if token and userId are present - respond with 200")
+    final void testLoginResponse(){
+
+        //What we want to send in the login body
+        Map<String, String> loginCredentials = new HashMap<>();
+        loginCredentials.put("email", "esther@gmail.com");
+        loginCredentials.put("password", "12345678");
+
+
+        Response response =
+                given()
+                        .contentType("application/json")
+                        .accept("application/json")
+                        .body(loginCredentials)
+                .when()
+                        .post(context_path + "/login")
+                .then()
+                        .statusCode(200)
+                .extract()
+                        .response();
+
+
+        String token = response.header("token");
+        String userId = response.header("userId");
+
+        //Assert that token and userId are present
+        assertNotNull(token);
+        assertNotNull(userId);
+    }
 }
