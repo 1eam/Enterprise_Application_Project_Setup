fd6ff2fc16e30ea35ddc01b6a8e225c906f8defb
Esther <esther191299@gmail.com>
2021-01-12 01:19:26 +0100

    added api address test stub

--- Rest Api Tests Service/src/test/java/com/enterpriseapp/restapitestsservice/usersService/statusCheckTest.java
+++ Rest Api Tests Service/src/test/java/com/enterpriseapp/restapitestsservice/usersService/statusCheckTest.java
@@ -1,4 +1,13 @@
 package com.enterpriseapp.restapitestsservice.usersService;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+
 public class statusCheckTest {
+    private final String CONTEXT_PATH = "users-service-api";
+    @BeforeEach
+    void setup() throws Exception{
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 54963;
+    }
 }
