fb8673df12c8ea928508dba9a8f4b1d3595b45f5
Esther <esther191299@gmail.com>
2021-01-12 01:41:32 +0100

    1st stub statusCheckTest with REST-Assured, not ran yet

--- Rest Api Tests Service/src/test/java/com/enterpriseapp/restapitestsservice/usersService/statusCheckTest.java
+++ Rest Api Tests Service/src/test/java/com/enterpriseapp/restapitestsservice/usersService/statusCheckTest.java
@@ -1,13 +1,34 @@
 package com.enterpriseapp.restapitestsservice.usersService;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class statusCheckTest {
-    private final String CONTEXT_PATH = "users-service-api";
+
+    private final String context_path = "users-service-api";
+
     @BeforeEach
     void setup() throws Exception{
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 54963;
+    }
+
+    @Test
+    final void testApplicationStatusResponse(){
+        Response response = given()
+                .contentType("application/json")
+                .accept("application/json")
+        .when()
+                .get(context_path + "/status")
+        .then()
+                .statusCode(200)
+                .contentType("application/json")
+        .extract()
+                .response();
+
     }
 }
