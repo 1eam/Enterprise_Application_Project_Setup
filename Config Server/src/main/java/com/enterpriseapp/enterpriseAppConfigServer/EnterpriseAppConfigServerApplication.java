243e887560807794b6f6d6a230b5a37916acb6fe
Esther <esther191299@gmail.com>
2021-01-13 04:47:51 +0100

    initial commit for Spring Cloud Config Server

---
+++ Config Server/src/main/java/com/enterpriseapp/enterpriseAppConfigServer/EnterpriseAppConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.enterpriseapp.enterpriseAppConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseAppConfigServerApplication.class, args);
+    }
+
+}
---
+++ Config Server/src/test/java/com/enterpriseapp/enterpriseAppConfigServer/EnterpriseAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterpriseapp.enterpriseAppConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAppConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
