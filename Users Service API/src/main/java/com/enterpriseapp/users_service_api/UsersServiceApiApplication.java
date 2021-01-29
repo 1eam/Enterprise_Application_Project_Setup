c2cf381f69962a1484be65c1f13f8ed835832811
Esther <esther191299@gmail.com>
2021-01-29 07:25:07 +0100

    Added @EnableFeignClient annotation to main class

--- Users Service API/src/main/java/com/enterpriseapp/users_service_api/UsersServiceApiApplication.java
+++ Users Service API/src/main/java/com/enterpriseapp/users_service_api/UsersServiceApiApplication.java
@@ -3,11 +3,13 @@ package com.enterpriseapp.users_service_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class UsersServiceApiApplication {
 
     public static void main(String[] args) {
