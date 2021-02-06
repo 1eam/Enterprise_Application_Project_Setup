68353b031edcb66f935beba0b1f0fc483aef1d42
Esther <esther191299@gmail.com>
2021-02-06 15:07:47 +0100

    formatted spaces and ordering in Profile Pictures Service, ProfilePicturesController.java file

--- Profile-Pictures Service API/src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/ProfilePicturesController.java
+++ Profile-Pictures Service API/src/main/java/com/appsdeveloperblog/photoapp/api/albums/io/controllers/ProfilePicturesController.java
@@ -8,39 +8,37 @@ package com.appsdeveloperblog.photoapp.api.albums.io.controllers;
 import com.appsdeveloperblog.photoapp.api.albums.data.ProfilePictureEntity;
 import com.appsdeveloperblog.photoapp.api.albums.service.ProfilePicturesService;
 import com.appsdeveloperblog.photoapp.api.albums.ui.model.ProfilePictureResponseModel;
-import java.util.ArrayList;
-import java.util.List;
 import org.modelmapper.ModelMapper;
-import java.lang.reflect.Type;
 import org.modelmapper.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 @RestController
 @RequestMapping("/users/{id}/profile-pictures")
 public class ProfilePicturesController {
-    
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     ProfilePicturesService profilePicturesService;
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-    @GetMapping( 
-            produces = { 
-                MediaType.APPLICATION_JSON_VALUE,
-                MediaType.APPLICATION_XML_VALUE,
-            })
-    public List<ProfilePictureResponseModel> userProfilePictures(@PathVariable String id) {
 
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,})
+
+    public List<ProfilePictureResponseModel> userProfilePictures(@PathVariable String id) {
         List<ProfilePictureResponseModel> returnValue = new ArrayList<>();
-        
         List<ProfilePictureEntity> photoEntities = profilePicturesService.getProfilePictures(id);
         
-        if(photoEntities == null || photoEntities.isEmpty())
-        {
+        if(photoEntities == null || photoEntities.isEmpty()) {
             return returnValue;
         }
         
