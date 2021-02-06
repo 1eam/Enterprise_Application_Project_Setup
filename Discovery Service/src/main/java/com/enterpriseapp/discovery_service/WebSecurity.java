4e69830e32731429dc98437864aa8b959f636fa8
Esther <esther191299@gmail.com>
2021-02-06 07:59:00 +0100

    Secured Eureka Dashboard, added new dependencies, Placed and Encrypted Login-credentials in private/distributed properties-files, Editted spring.cloud.config.name property of UsersService to Its own name

--- Discovery Service/src/main/java/com/enterpriseapp/discovery_service/WebSecurity.java
+++ Discovery Service/src/main/java/com/enterpriseapp/discovery_service/WebSecurity.java
@@ -1,8 +1,10 @@
 package com.enterpriseapp.discovery_service;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
